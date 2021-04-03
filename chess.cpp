dc1051cdb492b4aa939a33a2b322f3a2a5fe2d59
FadeHack <80592945+FadeHack@users.noreply.github.com>
2021-04-03 23:51:06 +0530

    Add files via upload

---
+++ chess.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#define HEIGHT 25
+#define WIDTH 48
+using namespace std;
+ 
+class chess_board
+{
+private:
+    friend class chess_pieces;
+public:
+ chess_board()
+ {
+    for (int i = 0; i < HEIGHT; i++)
+    {   cout << "#"<<endl ;
+        for (int j = 0; j < WIDTH; j++)
+        {
+            if (i == 0 || i == (HEIGHT-1) || (j == 0 && i) || i%3 == 0 || j%6 == 0)
+            {
+                cout << "#" ;
+            }
+            else
+            {
+                cout << " ";
+            }
+           
+        }
+        
+    }
+ }
+};
+
+// class chess_pieces
+// {
+
+
+// };
+
+
+int main()
+{
+        chess_board board;
+    return 0;
+}
